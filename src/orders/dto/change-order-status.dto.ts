6f7878587daf95cf7499bade6a44b49dbd87726a
JJRoca <hanzoblade1234@outlook.com>
2024-05-08 13:26:08 -0400

    Add paginationDto and configure env variables

---
+++ src/orders/dto/change-order-status.dto.ts
@@ -0,0 +1,12 @@
+import { IsEnum, IsUUID } from 'class-validator';
+import { orderStatusList } from '../enum/order.enum';
+import { OrderStatus } from '@prisma/client';
+
+export class ChangeOrderStatusDto {
+  @IsUUID()
+  id: string;
+  @IsEnum(orderStatusList, {
+    message: `valid status are ${orderStatusList}`,
+  })
+  status: OrderStatus;
+}
---
+++ src/orders/enum/order.enum.ts
@@ -0,0 +1,7 @@
+import { OrderStatus } from '@prisma/client';
+
+export const orderStatusList = [
+  OrderStatus.CANCELLED,
+  OrderStatus.DELIVERED,
+  OrderStatus.PENDING,
+];
