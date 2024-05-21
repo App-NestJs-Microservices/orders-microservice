1bef361bfaf26aa53010f1bc54928f5656350d11
JJRoca <hanzoblade1234@outlook.com>
2024-05-21 17:26:15 -0400

    add ordersDetail

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,13 +3,16 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
-  //DATABASEURL: string;
+  PRODUCTS_MICROSERVICE_HOST: string;
+  PRODUCTS_MICROSERVICE_PORT: number;
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
     //DATABASE_URL: joi.string().required(),
+    PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
+    PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
   })
   .unknown(true);
 
@@ -23,4 +26,6 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   //databaseUrl: envVars.DATABASEURL,
+  productsMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
+  productsMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
 };
