d54852272a60c6a36be6f78ea41aefb5dbb374e0
JJRoca <hanzoblade1234@outlook.com>
2024-07-11 17:25:32 -0400

    add model to schema.prima and add functions

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -9,17 +9,19 @@ import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from '@prisma/client';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { OrderPaginationDto } from './dto/order-pagination.dto';
-import { ChangeOrderStatusDto } from './dto';
+import { ChangeOrderStatusDto, PaidOrderDto } from './dto';
 import { NATS_SERVICE } from 'src/config';
 import { firstValueFrom } from 'rxjs';
+import { OrderWithProducts } from './interfaces/order-with-products.interface';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
-  private readonly logger = new Logger('Orders-Service');
+  private readonly logger = new Logger('OrdersService');
 
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {
     super();
   }
+
   async onModuleInit() {
     await this.$connect();
     this.logger.log('Database connected');
@@ -27,13 +29,13 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
 
   async create(createOrderDto: CreateOrderDto) {
     try {
-      //1 check ids from productos
+      //1 Confirmar los ids de los productos
       const productIds = createOrderDto.items.map((item) => item.productId);
       const products: any[] = await firstValueFrom(
         this.client.send({ cmd: 'validate_products' }, productIds),
       );
 
-      //2. Calculate the values
+      //2. Cálculos de los valores
       const totalAmount = createOrderDto.items.reduce((acc, orderItem) => {
         const price = products.find(
           (product) => product.id === orderItem.productId,
@@ -45,7 +47,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
         return acc + orderItem.quantity;
       }, 0);
 
-      //3.Create a database transaction
+      //3. Crear una transacción de base de datos
       const order = await this.order.create({
         data: {
           totalAmount: totalAmount,
@@ -91,10 +93,14 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
 
   async findAll(orderPaginationDto: OrderPaginationDto) {
     const totalPages = await this.order.count({
-      where: { status: orderPaginationDto.status },
+      where: {
+        status: orderPaginationDto.status,
+      },
     });
+
     const currentPage = orderPaginationDto.page;
     const perPage = orderPaginationDto.limit;
+
     return {
       data: await this.order.findMany({
         skip: (currentPage - 1) * perPage,
@@ -113,7 +119,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
 
   async findOne(id: string) {
     const order = await this.order.findFirst({
-      where: { id: id },
+      where: { id },
       include: {
         OrderItem: {
           select: {
@@ -124,16 +130,19 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
         },
       },
     });
+
     if (!order) {
       throw new RpcException({
         status: HttpStatus.NOT_FOUND,
         message: `Order with id ${id} not found`,
       });
     }
+
     const productIds = order.OrderItem.map((orderItem) => orderItem.productId);
     const products: any[] = await firstValueFrom(
       this.client.send({ cmd: 'validate_products' }, productIds),
     );
+
     return {
       ...order,
       OrderItem: order.OrderItem.map((orderItem) => ({
@@ -143,15 +152,58 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
       })),
     };
   }
-  async changeOrderStatus(changeOrderStatusDto: ChangeOrderStatusDto) {
+
+  async changeStatus(changeOrderStatusDto: ChangeOrderStatusDto) {
     const { id, status } = changeOrderStatusDto;
+
     const order = await this.findOne(id);
     if (order.status === status) {
       return order;
     }
+
     return this.order.update({
       where: { id },
       data: { status: status },
     });
+  }
+
+  async createPaymentSession(order: OrderWithProducts) {
+    const paymentSession = await firstValueFrom(
+      this.client.send('create.payment.session', {
+        orderId: order.id,
+        currency: 'usd',
+        items: order.OrderItem.map((item) => ({
+          name: item.name,
+          price: item.price,
+          quantity: item.quantity,
+        })),
+      }),
+    );
+
+    return paymentSession;
+  }
+
+  async paidOrder(paidOrderDto: PaidOrderDto) {
+    this.logger.log('Order Paid');
+    this.logger.log(paidOrderDto);
+
+    const order = await this.order.update({
+      where: { id: paidOrderDto.orderId },
+      data: {
+        status: 'PAID',
+        paid: true,
+        paidAt: new Date(),
+        stripeChargeId: paidOrderDto.stripePaymentId,
+
+        // La relación
+        OrderReceipt: {
+          create: {
+            receiptUrl: paidOrderDto.receiptUrl,
+          },
+        },
+      },
+    });
+
+    return order;
   }
 }
